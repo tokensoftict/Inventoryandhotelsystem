2d40c4e6e5534ca990cf13b6ffed218695d665de
Yusuf Olatunji <yoyforever@gmail.com>
2022-09-07 09:43:58 +0100

    fix

--- database/migrations/2022_08_21_111023_create_supplier_credit_payment_history_table.php
+++ database/migrations/2022_08_21_111023_create_supplier_credit_payment_history_table.php
@@ -19,12 +19,12 @@ class CreateSupplierCreditPaymentHistoryTable extends Migration
             $table->foreignId("user_id")->nullable()->constrained()->nullOnDelete();
             $table->unsignedBigInteger("supplier_id")->nullable();
             $table->foreignId("purchase_order_id")->nullable()->constrained()->cascadeOnDelete();
-            $table->foreignId("payment_method_id")->nullable()->constrained()->nullOnDelete();
+            $table->unsignedBigInteger("payment_method_id")->nullable();
             $table->string('payment_info')->nullable();
             $table->decimal("amount",20,5);
             $table->date("payment_date");
             $table->timestamps();
-            $table->foreign('payment_method_id')->references('id')->on('payment_method_table')
+            $table->foreign('payment_method_id')->references('id')->on('payment_method')
                 ->nullOnDelete();
             $table->foreign('supplier_id')->references('id')->on('supplier')->cascadeOnDelete();
         });
