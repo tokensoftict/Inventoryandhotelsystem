3a0310ccb47c14a59ec1adfeca8e6136df8e038a
piouskenny <kehindeu13@gmail.com>
2023-07-14 21:43:04 +0100

    added  table migration and basic route for the customer_table

--- app/Http/Controllers/Settings/CustomerTableController.php
+++ app/Http/Controllers/Settings/CustomerTableController.php
@@ -3,9 +3,66 @@
 namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
+use App\Models\CustomerTable;
 use Illuminate\Http\Request;
 
 class CustomerTableController extends Controller
 {
-    //
+    public function index()
+    {
+        $data['title'] = "List Expenses Type";
+        $data['title2'] = "Add Expenses Type";
+        $data['expenses_types'] = CustomerTable::all();
+        return setPageContent('settings.expenses_type.list-types', $data);
+    }
+
+
+    public function create()
+    {
+    }
+
+
+
+    public function store(Request $request)
+    {
+
+        $request->validate(CustomerTable::$validate);
+
+        $data = $request->only(CustomerTable::$fields);
+
+        // $data['status'] = 1;
+
+        CustomerTable::create($data);
+
+        return redirect()->route('expenses_type.index')->with('success', 'Expenses type as been created successful!');
+    }
+
+
+    public function toggle($id)
+    {
+
+        $this->toggleState(CustomerTable::find($id));
+
+        return redirect()->route('expenses_type.index')->with('success', 'Operation successful!');
+    }
+
+
+    public function edit($id)
+    {
+        $data['title'] = "Update Expenses Type";
+        $data['expenses_type'] = CustomerTable::find($id);
+        return setPageContent('settings.expenses_type.edit', $data);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $request->validate(CustomerTable::$validate);
+
+        $data = $request->only(CustomerTable::$fields);
+
+        CustomerTable::find($id)->update($data);
+
+        return redirect()->route('expenses_type.index')->with('success', 'Expenses Type as been updated successful!');
+    }
 }
--- app/Models/CustomerTable.php
+++ app/Models/CustomerTable.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class CustomerTable extends Model
 {
     use HasFactory;
+
+    protected $table = 'customer_table';
+    
+	protected $fillable = [
+		'name',
+		'department'
+	];
+
+    public static $fields = [
+        'name',
+        'department'
+    ];
+
+    public static $validate = [
+        'name'=>'required',
+        'department' => 'required'
+    ];
 }
