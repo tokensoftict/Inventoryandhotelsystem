7a1fd9f8c1e48389a72f2c008173adb2a9b52646
piouskenny <kehindeu13@gmail.com>
2023-07-15 15:45:58 +0100

    added the functionality and the view for the editing table

--- storage/framework/views/9d1ee28b59802b7ab6e4f6276a5605a815fae829.php
+++ storage/framework/views/9d1ee28b59802b7ab6e4f6276a5605a815fae829.php
@@ -1,1 +1,39 @@
-<?php /**PATH C:\xampp\htdocs\TokenSoft\Inventoryandhotelsystem\resources\views/settings/customer_table/edit.blade.php ENDPATH**/ ?>
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="ui-container">
+        <div class="row">
+                <div class="col-md-4">
+                    <section class="panel">
+                        <header class="panel-heading">
+                            <?php echo e($title); ?>
+
+                        </header>
+                        <div class="panel-body">
+                            <form id="validate" action="<?php echo e(route('customer_table.update',$expenses_type->id)); ?>" enctype="multipart/form-data" method="post">
+                                <?php echo e(csrf_field()); ?>
+
+                                <?php echo e(method_field('PUT')); ?>
+
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" value="<?php echo e(old('name', $expenses_type->name)); ?>" required  class="form-control" name="name" placeholder="Product Category"/>
+                                    <?php if($errors->has('name')): ?>
+                                        <label for="name-error" class="error"
+                                               style="display: inline-block;"><?php echo e($errors->first('name')); ?></label>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="pull-left">
+                                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Update</button>
+                                </div>
+                                <br/> <br/>
+                            </form>
+                        </div>
+                    </section>
+                </div>
+        </div>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\TokenSoft\Inventoryandhotelsystem\resources\views/settings/customer_table/edit.blade.php ENDPATH**/ ?>
