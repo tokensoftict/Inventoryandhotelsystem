5f70d5ce0170e85d0bb21bab2cf4e90f0d31190d
piouskenny <kehindeu13@gmail.com>
2023-07-18 22:15:11 +0100

    users can now select tables from the invoice of the table

--- app/Http/Controllers/TableInvoice/TableInvoiceController.php
+++ app/Http/Controllers/TableInvoice/TableInvoiceController.php
@@ -3,19 +3,31 @@
 namespace App\Http\Controllers\TableInvoice;
 
 use App\Http\Controllers\Controller;
+use App\Models\BankAccount;
+use App\Models\Customer;
+use App\Models\CustomerTable;
+use App\Models\PaymentMethod;
 use Illuminate\Http\Request;
 
 class TableInvoiceController extends Controller
 {
+    // TO create new invoice 
     public function new()
     {
-
+        $data = [];
+        $data['customers'] = Customer::all();
+        $data['payments'] = PaymentMethod::all();
+        $data['tables'] = CustomerTable::all();
+        $data['banks'] = BankAccount::where('status',1)->get();
+        return setPageContent('tableinvoice.new-table-invoice',$data);
     }
+    // show all table invoice
     public function list() {
         
     }
 
+    // show Active tables 
     public function active() {
-        
+
     }
 }
