0c8133d09f47bf2bca197511373d5627debf88e5
piouskenny <kehindeu13@gmail.com>
2023-07-19 11:22:01 +0100

    added the migration and model for table Invoice

--- app/Http/Controllers/TableInvoice/TableInvoiceController.php
+++ app/Http/Controllers/TableInvoice/TableInvoiceController.php
@@ -7,6 +7,7 @@ use App\Models\BankAccount;
 use App\Models\Customer;
 use App\Models\CustomerTable;
 use App\Models\PaymentMethod;
+use App\Models\TableInvoice;
 use Illuminate\Http\Request;
 
 class TableInvoiceController extends Controller
@@ -21,13 +22,20 @@ class TableInvoiceController extends Controller
         $data['banks'] = BankAccount::where('status',1)->get();
         return setPageContent('tableinvoice.new-table-invoice',$data);
     }
-    // show all table invoice
-    public function list() {
+
+    public function draft(){
+        $data = [];
+        $data['title'] = 'Draft Invoice List';
+        $data['tableinvoice'] = TableInvoice::with(['created_user','customer'])->where('warehousestore_id', getActiveStore()->id)->where('status','DRAFT')->where('invoice_date',date('Y-m-d'))->get();
+        return setPageContent('invoice.draft-invoice',$data);
+    }
+
+    // show all table invoice draft that has a particular Table ID and has a section to complete the payment
+    public function list() 
+    {
         
     }
 
     // show Active tables 
-    public function active() {
 
-    }
 }
